e02dce7be3a23f621190ba6956b7a33b8c55b57c
AnonGFSM <139561556+AnonGFSM@users.noreply.github.com>
2023-07-14 14:52:49 +0100

    Updating Build Folder

---
+++ build/CMakeFiles/GFSM.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GFSM.dir/HnGPU.cu.o"
+  "CMakeFiles/GFSM.dir/HnGPU.cu.o.d"
+  "CMakeFiles/GFSM.dir/ccsr.cpp.o"
+  "CMakeFiles/GFSM.dir/ccsr.cpp.o.d"
+  "CMakeFiles/GFSM.dir/cmake_device_link.o"
+  "CMakeFiles/GFSM.dir/main.cu.o"
+  "CMakeFiles/GFSM.dir/main.cu.o.d"
+  "GFSM"
+  "GFSM.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/GFSM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/GFSM.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GFSM.
+# This may be replaced when dependencies are built.
